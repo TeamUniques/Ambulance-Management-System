2d1a0c4d89e07e39dfeaf3a264e2148e160a0668
Naveengali <naveengali80@gmail.com>
2021-05-22 11:36:32 +0530

    Done Adding some Changes

--- home.php
+++ home.php
@@ -1,5 +1,8 @@
 <?php
     require ("assets/common.php");
+    if (!isset($_SESSION['email'])) {
+        header("Location:index.php");
+    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,6 +16,41 @@
     <?php
         require("header.php");
     ?>
+    <div class="container-fluid" style="margin:-15px;margin-top:-20px">
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                <div class="item active">
+                    <img src="images/amb1.jpg" alt="Los Angeles" style="width:100%;height:600px">
+                </div>
+
+                <div class="item">
+                    <img src="images/amb2.jpg" alt="Chicago" style="width:100%;height:600px">
+                </div>
+                
+                <div class="item">
+                    <img src="images/amb3.jpg" alt="New york" style="width:100%;height:600px">
+                </div>
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+                </a>
+        </div>
+    </div>
     </body>
 </html>
 
--- index.php
+++ index.php
@@ -1,17 +1,55 @@
-<?php
-    require ("assets/common.php");
-    ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Index | Ambulance Management System</title>
-        <?php
-            require ("assets/bootstrap_cdn.php");
+    <?php
+        require ("assets/common.php");
+        if (isset($_SESSION['email'])) {
+            header("Location:home.php");
+        }
         ?>
-    </head>
-    <body>
-        <?php
-            require("header.php");
-        ?>
-    </body>
-</html>
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Index | Ambulance Management System</title>
+            <?php
+                require ("assets/bootstrap_cdn.php");
+            ?>
+        </head>
+        <body>
+            <?php
+                require("header.php");
+            ?>
+            <div class="container-fluid" style="margin:-15px;margin-top:-20px">
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                <div class="item active">
+                    <img src="images/amb1.jpg" alt="Los Angeles" style="width:100%;height:600px">
+                </div>
+
+                <div class="item">
+                    <img src="images/amb2.jpg" alt="Chicago" style="width:100%;height:600px">
+                </div>
+                
+                <div class="item">
+                    <img src="images/amb3.jpg" alt="New york" style="width:100%;height:600px">
+                </div>
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+                </a>
+        </div>
+    </div>
+        </body>
+    </html>
